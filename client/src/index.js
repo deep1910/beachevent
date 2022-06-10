8210a666c081a8d677b0b5cdbef60b0ec9aed72c
snew430 <seanpnew@hotmail.com>
2022-06-10 17:17:27 -0400

    added queries

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
-import App from './App';
-// import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
+import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
-  <App />
-  </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 );
 
